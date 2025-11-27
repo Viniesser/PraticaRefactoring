e795c56af312ec8ba281f6f79a11079f079d61b1
Vinicius <vemarques@minha.fag.edu.br>
2025-11-27 09:04:22 -0300

    Commit 17

--- HtmlStatement.java
+++ HtmlStatement.java
@@ -1,20 +1,34 @@
 import java.util.Enumeration;
 
 public class HtmlStatement extends Statement {
+   
    public String value(Customer aCustomer) {
       Enumeration rentals = aCustomer.getRentals();
-      String result = "<H1>Rentals for <EM>" + aCustomer.getName() +
-      "</EM></H1><P>\n";
+      String result = headerString(aCustomer);
       while (rentals.hasMoreElements()) {
          Rental each = (Rental) rentals.nextElement();
-         result += each.getMovie().getTitle()+ ": " +
-         String.valueOf(each.getCharge()) + "<BR>\n";
+         result += eachRentalString(each);
       }
-      result += "<P>You owe <EM>" +
-      String.valueOf(aCustomer.getTotalCharge()) + "</EM><P>\n";
-      result += "On this rental you earned <EM>" + 
-      String.valueOf(aCustomer.getTotalFrequentRenterPoints()) +
-      "</EM> frequent renter points<P>";
+      result += footerString(aCustomer);
       return result;
+   }
+
+   private String headerString(Customer aCustomer) {
+       return "<H1>Rentals for <EM>" + aCustomer.getName() +
+       "</EM></H1><P>\n";
+   }
+
+   private String eachRentalString(Rental each) {
+       return each.getMovie().getTitle()+ ": " +
+       String.valueOf(each.getCharge()) + "<BR>\n";
+   }
+
+   private String footerString(Customer aCustomer) {
+       String result = "<P>You owe <EM>" +
+       String.valueOf(aCustomer.getTotalCharge()) + "</EM><P>\n";
+       result += "On this rental you earned <EM>" + 
+       String.valueOf(aCustomer.getTotalFrequentRenterPoints()) +
+       "</EM> frequent renter points<P>";
+       return result;
    }
 }
--- TextStatement.java
+++ TextStatement.java
@@ -1,20 +1,33 @@
 import java.util.Enumeration;
 
 public class TextStatement extends Statement {
+   
    public String value(Customer aCustomer) {
       Enumeration rentals = aCustomer.getRentals();
-      String result = "Rental Record for " + aCustomer.getName() +
-      "\n";
+      String result = headerString(aCustomer);
       while (rentals.hasMoreElements()) {
          Rental each = (Rental) rentals.nextElement();
-         result += "\t" + each.getMovie().getTitle()+ "\t" +
-         String.valueOf(each.getCharge()) + "\n";
+         result += eachRentalString(each);
       }
-      result += "Amount owed is " +
-      String.valueOf(aCustomer.getTotalCharge()) + "\n";
-      result += "You earned " +
-      String.valueOf(aCustomer.getTotalFrequentRenterPoints()) +
-      " frequent renter points";
+      result += footerString(aCustomer);
       return result;
+   }
+
+   private String headerString(Customer aCustomer) {
+       return "Rental Record for " + aCustomer.getName() + "\n";
+   }
+
+   private String eachRentalString(Rental each) {
+       return "\t" + each.getMovie().getTitle()+ "\t" +
+       String.valueOf(each.getCharge()) + "\n";
+   }
+
+   private String footerString(Customer aCustomer) {
+       String result = "Amount owed is " +
+       String.valueOf(aCustomer.getTotalCharge()) + "\n";
+       result += "You earned " +
+       String.valueOf(aCustomer.getTotalFrequentRenterPoints()) +
+       " frequent renter points";
+       return result;
    }
 }
